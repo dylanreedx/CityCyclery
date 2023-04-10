1f98ab2617f31e30a63f4ad9c04932be8f8e0832
Dylan <dylan.reeder5@gmail.com>
2023-04-10 08:22:47 -0400

    extended colors instead of just bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundColor: {
-        'light-500': '#EAE8FF',
-        'light-600': '#ADACB5',
-        'dark-500': '#2D3142',
-        'dark-600': '#212431',
-        'dark-700': '#171922',
-        'accent-500': '#B0D7FF',
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'light-500': '#EAE8FF',
+				'light-600': '#ADACB5',
+				'dark-500': '#2D3142',
+				'dark-600': '#212431',
+				'dark-700': '#171922',
+				'accent-500': '#B0D7FF',
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 };
