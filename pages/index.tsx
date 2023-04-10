34179792c698daa17d0b5adfc24c635f96f5d151
Dylan <dylan.reeder5@gmail.com>
2023-04-10 08:50:48 -0400

    base navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { HeadComponent } from '@/components/Head';
 import Image from 'next/image';
 import Button from '@/components/Button';
 import { Poppins } from '@next/font/google';
+import { Navbar } from '@/components/Navbar';
 
 // const poppins = Poppins({
 //   weight: '400',
@@ -15,15 +16,16 @@ export default function Home() {
 				title='Home - City Cyclery'
 				description='Sales, Service & Repair of Any & All Bicycles - Windsor, ON'
 			/>
+			<Navbar />
 			<main className='bg-dark-500'>
 				<section>
 					<h1>Sales, Service & Repair of Any & All Bicycles</h1>
 					<p>Proudly serving Windsor & Essex County</p>
 					<div>
-						<Button variant='primary' href='/contact'>
+						<Button variant='primary-base' href='/contact'>
 							Contact
 						</Button>
-						<Button variant='secondary' href='/services'>
+						<Button variant='secondary-base' href='/services'>
 							Contact
 						</Button>
 					</div>
