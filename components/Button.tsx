d1d5742ae0221a8b0bb061fd7d28c2145e194eb1
Dylan <dylan.reeder5@gmail.com>
2023-04-09 21:56:40 -0400

    rename and adjust sizes slightly

---
+++ components/Button.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import Link from 'next/link';
+
+const buttonStyle = cva(
+  'py-2 px-4 rounded flex-row items-center justify-evenly',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-accent-500',
+        secondary: 'bg-dark-600 text-white',
+      },
+    },
+  }
+);
+const buttonTextStyle = cva('font-poppins', {
+  variants: {
+    textSize: {
+      sm: 'text-xs',
+      base: 'text-sm',
+      lg: 'text-lg',
+    },
+  },
+});
+
+interface ButtonProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof buttonStyle>,
+    VariantProps<typeof buttonTextStyle> {
+  children: React.ReactNode;
+  variant: 'primary' | 'secondary';
+  icon?: React.ReactNode;
+  textSize?: 'sm' | 'base' | 'lg';
+  href?: any;
+  as?: any;
+}
+
+export default function Button({
+  children,
+  variant,
+  icon,
+  textSize,
+  href,
+  as,
+  ...props
+}: ButtonProps) {
+  return (
+    <Link href={href} as={as} passHref legacyBehavior>
+      <a {...props} className={buttonStyle({ variant })}>
+        {icon && icon}
+        <span className={buttonTextStyle({ textSize })}>{children}</span>
+      </a>
+    </Link>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { HeadComponent } from '@/components/Head';
 import Image from 'next/image';
-import Button from '@/components/Buttons';
+import Button from '@/components/Button';
 import { Poppins } from '@next/font/google';
 
 // const poppins = Poppins({
@@ -12,12 +12,16 @@ export default function Home() {
   return (
     <>
       <HeadComponent
-        title = {'Home - City Cyclery'}
-        description = {'Sales, Service & Repair of Any & All Bicycles - Windsor, ON'}
+        title={'Home - City Cyclery'}
+        description={
+          'Sales, Service & Repair of Any & All Bicycles - Windsor, ON'
+        }
       ></HeadComponent>
       <main className='bg-dark-500'>
         <h1 className='text-red-200'>test</h1>
-        <Button href='/services' variant='primary' textSize='base'>Services</Button>
+        <Button href='/services' variant='primary' textSize='base'>
+          Services
+        </Button>
       </main>
     </>
   );
