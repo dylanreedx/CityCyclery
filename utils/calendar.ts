96a1097728aaeb6238266f0d1058de1e83f9b2e3
alexsouchereau <W0632604@myscc.ca>
2023-04-11 14:09:46 -0400

    Added calendar link

---
+++ utils/calendar.ts
@@ -0,0 +1,4 @@
+// will eventually use an array of calendar links, this is fine for now
+
+// This is a public calendar with test data, don't put any private calendar links here
+export const calendar = 'https://calendar.google.com/calendar/ical/52accc6c86ba937a91f2f009bd0a58127ed1746928d2ef5105de19f3bdc6eeaf%40group.calendar.google.com/public/basic.ics';
