3185d687e1f2ae5063a65935666e86ac45d2e407
langleynick <w0106967@myscc.ca>
2023-04-11 18:26:03 -0400

    Formatted 'Services' page

---
+++ utils/services-data.ts
@@ -0,0 +1,59 @@
+import { CardProps } from '@/components/CardComponent'
+
+export const builds : CardProps []= [
+    {
+        title: 'Bike Build',
+        price: 100,
+        description: "This is the description of the build.This is the description of the build."
+    },
+    {
+        title: 'E-Bike Build',
+        price: 150,
+        description: "This is the description of the build."
+    },
+    {
+        title: "Trike Build",
+        price: 250,
+        description: "This is the description of the build."
+    }
+];
+export const tunes : CardProps [] = [
+    {
+        title: 'Bike Tune',
+        price: 90,
+        description: "This is the description of the tune."
+    },
+    {
+        title: 'Bike Wash-Bold Tighten',
+        price: 35,
+        description: "This is the description of the wash and bolt tighten."
+    }
+];
+export const installs : CardProps[] = [
+    {
+        title: 'install 1',
+        description: 'This is the description of the install'
+    },
+    {
+        title: 'install 2',
+        description: 'This is the description of the install'
+    },
+    {
+        title: 'install 3',
+        description: 'This is the description of the install'
+    }
+];
+export const adjustments : CardProps[] = [
+    {
+        title: 'adjustment 1',
+        description: 'This is the description of the adjustment'
+    },
+    {
+        title: 'adjustment 2',
+        description: 'This is the description of the adjustment'
+    },
+    {
+        title: 'adjustment 3',
+        description: 'This is the description of the adjustment'
+    }
+];
