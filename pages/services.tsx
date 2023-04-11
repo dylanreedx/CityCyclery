af465ebff0d57aa4f695a0133a126601a5ff3dc2
Angela <w0796187@myscc.ca>
2023-04-10 20:40:47 -0400

    builds edit

--- pages/services.tsx
+++ pages/services.tsx
@@ -9,13 +9,10 @@ import { Layout } from '@/components/Layout';
 export default function Services() {
 	return (
 		<>
-			{/* <HeadComponent
+			<HeadComponent
 				title='Services - City Cyclery'
 			/>
 			<Layout>
-				<div></div>
-			</Layout> */}
-            <section>
                 <h1 className='text-light-500 font-bold text-4xl md:text-6xl z-50 max-w-[20ch]'>
 				Services
 			    </h1>
@@ -23,7 +20,7 @@ export default function Services() {
                 <h4>Bike Build</h4>
                 <p>$100.00</p>
                 <p>This is the description of the bike biuld.</p>
-            </section>
+			</Layout>
 		</>
 	);
 }
