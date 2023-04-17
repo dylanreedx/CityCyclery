43dae9b3f9c4d5641ca12bb38ea7596c9a60a238
Angela <w0796187@myscc.ca>
2023-04-17 17:49:47 -0400

    Epic Cycles description

--- utils/brands-data.ts
+++ utils/brands-data.ts
@@ -23,7 +23,7 @@ export const houseBrands : BrandProps[] = [
 			'https://images.unsplash.com/photo-1501147830916-ce44a6359892?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 		url:'https://epiccycles.ca',
 		description:
-			'Specialized is a Taiwanese bicycle manufacturer based in Taichung, Taiwan. The company was founded in 1972 by King Liu and is currently owned by the Giant Group. Giant is known for its high-end mountain bikes and road bikes.',
+			'Located in the GTA, Epic Cycles is Canadas Premier Distributor and Electric Bikes and E-Scooters. Their goal is to change the way people travel by promoting e-bike riding as a safe, green alternative to other modes of transportation. ',
 	}
 ];
 export const distBrands : BrandProps[] = [
