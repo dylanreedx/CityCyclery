79bef66e3728b66bcee8eb938e8888f4bbd57fe2
alexsouchereau <W0632604@myscc.ca>
2023-04-21 15:27:36 -0400

    Made events page mobile responsive

--- pages/events.tsx
+++ pages/events.tsx
@@ -13,14 +13,16 @@ export default function Events() {
 		<>
 			<Layout title='Events - City Cyclery' description='Upcoming events'>
 				<div className='mt-16 max-w-5xl m-auto p-4 mb-4'>
-					<div className='flex flex-row gap-3 text-light-500 my-16 items-baseline'>
-						<h1 className='font-bold text-4xl'>Upcoming Events</h1>
-						<Calendar size={32} />
+					<div className='flex flex-col gap-3 text-light-500 my-16 items-baseline flex-wrap'>
+						<div className='flex flex-row gap-3 items-baseline'>
+							<h1 className='font-bold text-4xl'>Upcoming Events</h1>
+							<Calendar size={32} />
+						</div>
 						<span className='font-medium text-xs text-light-500 block'>
 							All times are based on timezone: {timezone}.
 						</span>
 					</div>
-					<div className='grid grid-cols-2 gap-8'>
+					<div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
 						{events.data ? (
 							events.data.map((event: any) => (
 								<div
