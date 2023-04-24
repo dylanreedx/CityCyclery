b2edbe146af34b7db74f55c4e43478384f300d09
Dylan <dylan.reeder5@gmail.com>
2023-04-24 10:20:12 -0400

    added overlay and zoom control position

--- components/MapsComponent.tsx
+++ components/MapsComponent.tsx
@@ -1,49 +1,49 @@
-import { companyDetails } from "@/utils/company-details";
-import { useLoadScript, GoogleMap, MarkerF } from "@react-google-maps/api";
+import { companyDetails } from '@/utils/company-details';
+import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
 import { useMemo } from 'react';
 
 const Map = () => {
-    const mapCenter = useMemo(
-        () => ({lat: 42.3216447, lng: -83.0000997}),
-        []
-    );
-    
-    const mapOptions = useMemo<google.maps.MapOptions>(
-        () => ({
-            disableDefaultUI: true,
-            scrollwheel: false,
-            keyboardShortcuts: false,
-            noClear: true,
-            overviewMapControl: false,
-            zoomControl: true,
-            backgroundColor: 'bg-dark-500',
-        }),
-        []
-    );
+	const mapCenter = useMemo(() => ({ lat: 42.3216447, lng: -83.0000997 }), []);
 
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string
-    });
-    
-    if(!isLoaded) {
-        return(
-            <div className="text-center m-auto">
-                <p className="text-light-500 mt-44">Loading map...</p>
-            </div>
-        )
-    }
+	const mapOptions = useMemo<google.maps.MapOptions>(
+		() => ({
+			disableDefaultUI: true,
+			scrollwheel: false,
+			keyboardShortcuts: false,
+			noClear: true,
+			overviewMapControl: false,
+			zoomControl: true,
+			backgroundColor: 'bg-dark-500',
+			zoomControlOptions: {
+				position: 8.0,
+			},
+		}),
+		[],
+	);
 
-    return (
-        <div className="flex m-auto w-full h-full">
-            <GoogleMap
-                options={mapOptions}
-                zoom={20}
-                center={mapCenter}
-                mapTypeId={google.maps.MapTypeId.ROADMAP}
-                mapContainerStyle={{ width: '1900px', height: '550px'}}
-            >
-            </GoogleMap>
-        </div>
-    )
-}
-export default Map
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+	});
+
+	if (!isLoaded) {
+		return (
+			<div className='text-center m-auto'>
+				<p className='text-light-500 mt-44'>Loading map...</p>
+			</div>
+		);
+	}
+
+	return (
+		<div className='flex m-auto w-full h-full relative'>
+			<div className='absolute inset-0 bg-gradient-to-t from-black to-transparent pointer-events-none'></div>
+			<GoogleMap
+				options={mapOptions}
+				zoom={20}
+				center={mapCenter}
+				mapTypeId={google.maps.MapTypeId.ROADMAP}
+				mapContainerStyle={{ width: '1900px', height: '550px' }}
+			></GoogleMap>
+		</div>
+	);
+};
+export default Map;
