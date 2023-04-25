e6493d0bcae629fb95215a45a42f3992fbb53cc4
Angela <w0796187@myscc.ca>
2023-04-25 14:29:35 -0400

    added calendar link to test

--- utils/calendar.ts
+++ utils/calendar.ts
@@ -1,3 +1,3 @@
 // This is a public calendar with test data, don't put any private calendar links here
 export const calendar =
-	'https://calendar.google.com/calendar/ical/52accc6c86ba937a91f2f009bd0a58127ed1746928d2ef5105de19f3bdc6eeaf%40group.calendar.google.com/public/basic.ics';
+	'https://calendar.google.com/calendar/ical/f434b7f1277197598e687aaff2e5cdcb1058aacc9a3d5bde10c06c2b9bb0f1c0%40group.calendar.google.com/public/basic.ics';
